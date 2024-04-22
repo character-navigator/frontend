d134272711cda48f044f26b2887d6058426618c4
Daniel Dahlberg <daniel.dahlberg@bookbeat.com>
2024-04-22 16:48:52 +0200

    Highlighted character names, summary drawer, progress bars.

--- src/pages/Reader/ReaderView.tsx
+++ src/pages/Reader/ReaderView.tsx
@@ -1,18 +1,37 @@
 import React, {useState, useEffect } from 'react'
+import Drawer from '@mui/material/Drawer';
 import { ReactReader } from 'react-reader'
 import type { Contents, Rendition } from 'epubjs'
+import CharacterNavigatorIcon from '../../assets/character_navigator_icon.png'
 import './Reader.css'
 
 
 export const ReaderView = () => {
     const [location, setLocation] = useState<string | number>(0)
     const [rendition, setRendition] = useState<Rendition | undefined>(undefined)
-    
-    document.addEventListener("click", ()=>{
-      // document.querySelector(".character")?.classList.add("highlighted-character")
-      const test = document.querySelector(".character")
-      console.log(test)
-    })
+    const [openDrawer, setOpenDrawer] = useState(false)
+    const [summaryWithHighlightedCharacter, setSummaryWithHighlightedCharacter] = useState<any>()
+
+    const summary = "Mr. Jones is the owner of Animal Farm. He neglects the animals, spends most of his time drinking and reading the news " +
+    "paper and not feeding them. He is taken by surprise by the animals when they fight back against him and his men, so much " +
+    "so that he is thrown off the farm."
+
+    function renderSummaryWithHighlightedCharacter(characterName: string, summary: string) {
+      const characterIndex = summary.indexOf(characterName)
+      const summaryWithoutCharacter = summary.substring(characterIndex + characterName.length)
+
+      return <p style={{margin: '1vh 2vh'}}><span className='character-name'>{ characterName }</span>{ summaryWithoutCharacter }</p>
+    }
+
+    const count = 8
+    const barWidth = Math.floor(100 / count)
+    const bars = Array.from({ length: count }, (_, index) => index);
+    const barStyle = {  
+      backgroundColor: '#cd95ff',  
+      height: '1.5vw',
+      margin: '0 0.5vw',  
+      width: `${barWidth}%`, // Divide 100% by the number of divs to get equal width  
+    };  
 
     // useEffect(() => {
        
@@ -51,10 +70,27 @@ export const ReaderView = () => {
       if (rendition) {
         rendition.hooks.content.register((contents: any) => {
           const doc = contents.document;
-          doc.addEventListener("click", () => {
-            const characterElement = doc.querySelector(".character");
-            if (characterElement) {
-              characterElement.classList.add("highlighted-character");
+          doc.addEventListener("click", (e: React.MouseEvent) => {
+            const characterElement: Element = doc.querySelector(".character");
+            const targetElement = e.target as Element;
+            const highlightedCharacter = "highlighted-character"
+
+            if (!targetElement.classList.contains("character")) {
+              if (characterElement) {
+                console.log(characterElement)
+                if (characterElement.classList.contains(highlightedCharacter)) {
+                  characterElement.classList.remove(highlightedCharacter)
+                } else {
+                  characterElement.classList.add(highlightedCharacter)
+                }
+              }
+            } else {
+              setSummaryWithHighlightedCharacter(
+                renderSummaryWithHighlightedCharacter(targetElement.innerHTML, summary)
+              )
+              setOpenDrawer(true)
+              console.log(targetElement.innerHTML)
+              characterElement.classList.remove(highlightedCharacter)
             }
           });
         });
@@ -77,6 +113,29 @@ export const ReaderView = () => {
             setRendition(_rendition)
           }}
         />
+        <Drawer
+          anchor="bottom"
+          open={openDrawer}
+          onClose={() => setOpenDrawer(false)}>
+            <div className='character-navigator-copy'>
+              <span onClick={() => setOpenDrawer(false)} style={{fontSize: '4vw', display: 'flex', justifyContent: 'flex-end'}}>&#10005;</span>
+              <p style={{fontWeight: 'bold', marginTop: 0}}><img style={{height: '1.35vh', marginLeft: '4.3vw'}} src={CharacterNavigatorIcon}/>&nbsp; Character Navigator</p>
+              <p id="character-summary">
+              </p>
+              {summaryWithHighlightedCharacter}
+              <div style={{margin: '8vw 4vw 0 4vw'}}>
+                <div style={{ display: 'flex', width: '100%' }}>
+                  {bars.map((_, index) => (  
+                    <div key={index} style={barStyle} /> // Render each div with the specified style  
+                  ))}
+                </div>
+              </div>
+              <div style={{width: '100%', textAlign: 'center', marginTop: '1vh'}}>
+                Summary based on {barWidth}%
+              </div>
+            </div>
+            
+          </Drawer>
       </div>
     )
   }
