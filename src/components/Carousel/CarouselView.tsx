e472d1f927f71d3666035126b333c4f31a031929
Axel Lokrantz <axel.lokrantz@bookbeat.com>
2024-05-02 16:56:59 +0200

    changed carousel image

--- src/components/Carousel/CarouselView.tsx
+++ src/components/Carousel/CarouselView.tsx
@@ -1,8 +1,8 @@
 import { Slider, Slide } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import AnimalFarm from "../../assets/animal-farm-book-cover.jpg";
+import AnimalFarm from "../../assets/animal-farm.jpg";
+import Odyssey from "../../assets/odyssey.png";
 import ChristmasCarol from "../../assets/christmas-carol.png";
-import AliceInWonderland from "../../assets/alice-in-wonderland.png";
 import WarAndPeace from "../../assets/war-and-peace.png";
 import { Link } from "react-router-dom";
 
@@ -10,9 +10,9 @@ const books = [
   { title: "Animal Farm", author: "George Orwell", cover: AnimalFarm },
   { title: "War and Peace", author: "Leo Tolstoy", cover: WarAndPeace },
   {
-    title: "Alice in Wonderland",
-    author: "Lewis Carroll",
-    cover: AliceInWonderland,
+    title: "Odyssey",
+    author: "Homer",
+    cover: Odyssey,
   },
   {
     title: "A Christmas Carol",
